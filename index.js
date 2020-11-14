8e115b3dfdfbbb1beb817cf27fc476435fbd9894
panjiganzz <74246137+panjiganzz@users.noreply.github.com>
2020-11-15 06:36:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'NAMA BOTMU'; 
-const instagram = 'LINK INSTAGRAMU'; 
-const telegram = 'LINK TELEMU'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'LINK YTMU';
-//const grupch1 = 'belum ada grup'; 
+const BotName = 'XENON'; 
+const instagram = 'https://instagram.com/itsmegaragong'; 
+const telegram = 'GAPUNYA TELEGRAM'; 
+const kapanbotaktif = 'KETIKA ADA KUOTA'; 
+const youtube = 'https://youtube.com/panjiganz';
+//const grupch1 = 'https://chat.whatsapp.com/LGOV1KpRCDOLtQJAUnWiPs'; 
 //const grupch2 = 'belum ada grup' ; 
 const
 {
@@ -64,11 +64,11 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@reuz.id')
+conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@itsmegaragong')
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@itsmegaragong`)
 })
 
 conn.on('message-new', async(m) =>
